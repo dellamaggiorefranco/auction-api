ef78c25c2fbadf3d2106d35c17970869ee6c5ec5
Franco Dellamaggiore <dellamaggiorefranco1@gmail.com>
2026-06-29 10:49:47 -0300

    feat(role): Añadir restriccion de unicidad y no nulo al nombre del rol

--- src/main/java/com/subastas/subastas_api/entity/Role.java
+++ src/main/java/com/subastas/subastas_api/entity/Role.java
@@ -1,10 +1,6 @@
 package com.subastas.subastas_api.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,5 +16,6 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String name;
 }
